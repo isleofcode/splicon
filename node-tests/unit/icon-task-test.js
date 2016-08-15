c8b050afab9418dab65c7fe27eacb8e7be2eebbf
Jordan Yee <jordanyee@users.noreply.github.com>
2016-08-15 01:42:55 -0400

    test(icon-task): Add test

---
+++ node-tests/unit/icon-task-test.js
@@ -0,0 +1,113 @@
+'use strict';
+
+const expect        = require('../helpers/expect');
+const fs            = require('fs');
+const sizeOf        = require('image-size');
+const parseString   = require('xml2js').parseString;
+const _find         = require('lodash').find;
+const DefaultIcons  = require('../../src/default-icons');
+
+const IconTask      = require('../../src/icon-task');
+
+describe('IconTask', function() {
+  // Hitting the file system is slow
+  this.timeout(0);
+
+  const fixturePath = 'node-tests/fixtures/config.xml';
+  const tmpConfigPath = 'tmp/config.xml'
+
+  const svgPath = 'node-tests/fixtures/icon.svg';
+  const pngPath = 'icons';
+  const projectPath = 'tmp';
+
+  before(() => {
+    fs.createReadStream(`${fixturePath}/no-platform-nodes.xml`)
+    .pipe(fs.createWriteStream(tmpConfigPath));
+  });
+
+  after(() => {
+    fs.unlinkSync(tmpConfigPath);
+  });
+
+  context('when platforms', () => {
+    context('when ios', () => {
+      const platform = 'ios';
+      const platformSizes = DefaultIcons[platform];
+      let task;
+
+      before(() => {
+        task = IconTask({
+          source: svgPath,
+          dest: pngPath,
+          projectPath: projectPath,
+          platforms: [platform]
+        })
+      });
+
+      after(() => {
+        platformSizes.items.forEach((size) => {
+          const path = `${projectPath}/${pngPath}/${platform}/${size.name}.png`;
+          fs.unlinkSync(path);
+        });
+        fs.rmdirSync(`${projectPath}/${pngPath}/${platform}`);
+        fs.rmdirSync(`${projectPath}/${pngPath}`);
+      });
+
+      it('generates the icons', (done) => {
+        task.then(() => {
+          try {
+            platformSizes.items.forEach((size) => {
+              const path = `${projectPath}/${pngPath}/${platform}/${size.name}.png`;
+
+              expect(fs.existsSync(path)).to.equal(true);
+              expect(sizeOf(path).width).to.equal(size.size);
+              expect(sizeOf(path).height).to.equal(size.size);
+            });
+            done();
+          } catch(e) {
+            done(e);
+          }
+        });
+      });
+
+      it('updates config.xml', (done) => {
+        task.then(() => {
+          const configXML = fs.readFileSync(tmpConfigPath, 'utf8');
+
+          try {
+            parseString(configXML, (err, config) => {
+              if (err) done(err);
+
+              const platformNode = _find(config.widget.platform, (node) => {
+                return node.$.name == platform;
+              });
+
+              expect(platformNode).to.exist;
+
+              const iconsAttrs = platformNode.icon.map((iconNode) => {
+                return iconNode.$;
+              });
+
+              platformSizes.items.forEach((size) => {
+                const attrs = {
+                  src: `${pngPath}/${platform}/${size.name}.png`,
+                  height: size.size.toString(),
+                  width: size.size.toString()
+                }
+
+                expect(iconsAttrs).to.include(attrs);
+              });
+            });
+            done();
+          } catch(e) {
+            done(e);
+          }
+        });
+      });
+
+      it('returns a promise', (done) => {
+        expect(task).to.be.fulfilled.notify(done);
+      });
+    });
+  });
+});
